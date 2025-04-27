d51f33f0bb02da33de10df97629fa256699cd6e3
Nihita Lele <nihitalele@gmail.com>
2025-04-27 22:55:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var isDate = function (input) {
-  if (input instanceof Date && !isNaN(input)) {
+  if (input instanceof Date && !isNaN(input.getTime())) {
     return true;
   }
+  
   if (typeof input === "string" || typeof input === "number") {
     const parsedDate = new Date(input);
-    return !isNaN(parsedDate);
+    return !isNaN(parsedDate.getTime());
   }
+
   return false;
 };
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
-
-
 
